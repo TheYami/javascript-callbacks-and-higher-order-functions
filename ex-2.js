87c3dd592bfdbdd79f5f275226a6d98ce10b0a88
TheYami <luffy90san@gmail.com>
2024-10-08 14:10:58 +0700

    At Least Five Function

--- ex-2.js
+++ ex-2.js
@@ -2,6 +2,17 @@
 
 function atLeastFive(array, operation) {
   // Start coding here
+  let scoreMoreThan70 = 0;
+  for(let i = 0; i < array.length; i++){
+    if(operation(array[i])){
+      scoreMoreThan70++;
+    }
+  }
+  if(scoreMoreThan70 >= 5){
+    return "ผ่านเกณฑ์ ✅"
+  }else{
+    return "ไม่ผ่านเกณฑ์ ❌"
+  }
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
@@ -13,3 +24,22 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 let scoreRoom1Result;
 let scoreRoom2Result;
 let scoreRoom3Result;
+
+scoreRoom1Result = atLeastFive(studentScoresRoom1,function(score){
+  return score >= 70;
+});
+
+scoreRoom2Result = atLeastFive(studentScoresRoom2,function(score){
+  return score >= 70;
+});
+
+scoreRoom3Result = atLeastFive(studentScoresRoom3,function(score){
+  return score >= 70;
+});
+
+
+
+
+console.log(scoreRoom1Result);
+console.log(scoreRoom2Result);
+console.log(scoreRoom3Result);
