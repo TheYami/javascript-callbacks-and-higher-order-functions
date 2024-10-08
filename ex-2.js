add4d628536f0a8392ecac1addb9157ce84248b6
fifazii91 <fifa.settwut@gmail.com>
2024-10-08 14:32:23 +0700

    feat: Add JavaScript Callbacks and Higher Order Functions Assignment

--- ex-2.js
+++ ex-2.js
@@ -2,6 +2,18 @@
 
 function atLeastFive(array, operation) {
   // Start coding here
+  let passedCount = 0;
+  for (let score of array) {
+    passedCount += operation(score);
+  }
+  if (passedCount >= 5) {
+    return `ผ่านเกณฑ์ ✅`;
+  }
+  return `ไม่ผ่านเกณฑ์ ❌`;
+}
+
+function isPassed(score) {
+  return score > 70 ? 1 : 0;
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
@@ -10,6 +22,23 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = `นักเรียนห้องที่ 1 ${atLeastFive(
+  studentScoresRoom1,
+  isPassed
+)}`;
+
+let scoreRoom2Result = `นักเรียนห้องที่ 2 ${atLeastFive(
+  studentScoresRoom2,
+  isPassed
+)}`;
+
+let scoreRoom3Result = `นักเรียนห้องที่ 3 ${atLeastFive(
+  studentScoresRoom3,
+  isPassed
+)}`;
+
+console.log(scoreRoom1Result);
+
+console.log(scoreRoom2Result);
+
+console.log(scoreRoom3Result);
